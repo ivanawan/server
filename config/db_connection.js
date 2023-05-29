d456fbf6b4d6b2adfd23927d0e7cc806a961c3b2
ivan setiawan <ivansetiawanvll@gmail.com>
2023-05-29 20:57:13 +0700

    commit

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,9 +1,18 @@
 
  const Sequelize = require("sequelize");
 
- const sequelize = new Sequelize("FinalTask", "root", "", {
-     host: "127.0.0.1",
-     dialect: "mysql"
- });
+//  const sequelize = new Sequelize("FinalTask", "root", "", {
+//      host: "127.0.0.1",
+//      dialect: "mysql"
+//  });
+  const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+    }
+  );
  
  module.exports = sequelize;
--- index.js
+++ index.js
@@ -6,69 +6,73 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const httpServer = createServer(app);
 const bodyParser = require('body-parser')
-const port = 5000
-const io = new Server(httpServer, { cors: {origin: '*' } });
-const index_route = require('./src/routes/index');
-const sequelize = require('./config/db_connection');
-const index = require('./models/index');
-const {PurchasesBook}= require('./models/index');
+const port = 5200;
+const io = new Server(httpServer, { cors: { origin: "*" } });
+const index_route = require("./src/routes/index");
+const sequelize = require("./config/db_connection");
+const index = require("./models/index");
+const { PurchasesBook } = require("./models/index");
 
 /**
  *    +++++++++++++++   cors & header    +++++++++++++++
- */ 
+ */
 
- app.use(cors());
- app.use(function (req, res, next) {
-   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-   res.setHeader('Access-Control-Allow-Credentials', true);
-   next();
- });
- 
- /**
-  *    +++++++++++++++   body parser    +++++++++++++++
-  */ 
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
+/**
+ *    +++++++++++++++   body parser    +++++++++++++++
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /**
  *    +++++++++++++++   import route    +++++++++++++++
- */ 
+ */
 app.use("/public", express.static(__dirname + "/public"));
 httpServer.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
-app.get('/', (req, res) =>{res.send('Hello finaltask api v.1.00 !')});
-app.use('/api/v1',index_route);
-
+app.get("/", (req, res) => {
+  res.send("Hello finaltask api v.1.00 !");
+});
+app.use("/api/v1", index_route);
 
 /**
  *    +++++++++++++++   Socket IO    +++++++++++++++
- */ 
+ */
 
- require('./src/socket/index')(io)
+require("./src/socket/index")(io);
 
- 
 /**
  *    +++++++++++++++   db connection     +++++++++++++++
- */ 
+ */
 
- const initApp = async () => {
+const initApp = async () => {
   console.log("Testing the database connection..");
-   try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-       /**
-       * Syncronize the Post model.
-       */
-      // await sequelize.sync();
-      // await PurchasesBook.sync({force:true});
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    /**
+     * Syncronize the Post model.
+     */
+    await sequelize.sync();
+    // await PurchasesBook.sync({force:true});
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 };
 /**
 * Initialize the application.
